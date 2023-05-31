4790a8ebe34d0d34a2e98e7cd32e6cc15f868994
sjungwon <sjungwo03@gmail.com>
2023-05-31 19:32:13 +0900

    refactor: 예외 응답 포맷 추가

--- back/src/main/java/domhwangcha/voca/web/advice/ExceptionAdvice.java
+++ back/src/main/java/domhwangcha/voca/web/advice/ExceptionAdvice.java
@@ -1,6 +1,7 @@
 package domhwangcha.voca.web.advice;
 
 import domhwangcha.voca.exception.HttpException;
+import domhwangcha.voca.web.dto.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +13,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ExceptionAdvice{
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleBasicError(Exception e){
+    public ResponseEntity<ErrorResponse> handleBasicError(Exception e){
         log.info("internal server error", e);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(HttpStatus.INTERNAL_SERVER_ERROR.name());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.name()));
     }
     @ExceptionHandler(HttpException.class)
-    public ResponseEntity<String> handleHttpException(HttpException e){
-        return ResponseEntity.status(e.getHttpStatus()).body(e.getMessage());
+    public ResponseEntity<ErrorResponse> handleHttpException(HttpException e){
+        return ResponseEntity.status(e.getHttpStatus()).body(new ErrorResponse(e.getMessage()));
     }
 }
---
+++ back/src/main/java/domhwangcha/voca/web/dto/ErrorResponse.java
@@ -0,0 +1,13 @@
+package domhwangcha.voca.web.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+}
