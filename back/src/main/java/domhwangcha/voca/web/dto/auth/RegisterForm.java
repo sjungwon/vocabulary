7691c16405b552c4fcc2e9c8583c6398b5190ce7
sjungwon <sjungwo03@gmail.com>
2023-06-01 23:52:36 +0900

    refactor: 회원가입 정규표현식 변경

--- back/src/main/java/domhwangcha/voca/web/dto/auth/RegisterForm.java
+++ back/src/main/java/domhwangcha/voca/web/dto/auth/RegisterForm.java
@@ -13,11 +13,12 @@ import javax.validation.constraints.Size;
 public class RegisterForm {
     @NotBlank(message = "필수 값입니다")
     @Size(min=4, max = 10, message = "아이디를 4자 ~ 10자 사이로 입력해주세요.")
-    @Pattern(regexp = "^[a-z][a-z0-9]+$", message = "아이디는 영문자로 시작해야 하고, 영문자, 숫자만 입력 가능합니다.")
+    @Pattern(regexp = "^[a-zA-Z][a-zA-Z0-9]+$", message = "아이디는 소문자로 시작해야 하고, 소문자, 숫자만 입력 가능합니다.")
     private String account;
 
     @NotBlank(message = "필수 값입니다")
     @Size(min = 8, max = 20, message = "비밀번호를 8자 ~ 20자 사이로 입력해주세요.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "비밀번호는 영문자와 숫자를 각 1개 이상 포함해야합니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*[0-9])[\\d\\D]+$", message = "비밀번호는 영문자와 숫자를 각 1개 이상 포함해야합니다.")
+    @Pattern(regexp = "^[A-Za-z0-9@$!%*#?]+$", message = "비밀번호의 특수문자는 ^, @, $, !, %, *, #, ?만 가능합니다. ")
     private String password;
 }
